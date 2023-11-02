9de4ab0640d7c481415326936349d80cb7b84b28
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-02 17:13:19 +0100

    Refactor code using useState and useEffect

--- src/components/feedback/feedbackFn.js
+++ src/components/feedback/feedbackFn.js
@@ -1,78 +1,68 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Section } from './section/section';
 import { Statistics } from './statistics/statistics';
 import { Notification } from './notification/notification';
 import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    positive: 0,
-    totalFeedback: 0,
-    positivePercentage: 0,
-    showStatistics: false,
+const Feedback = () => {
+  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [totalFeedback, setTotalFeedback] = useState(0);
+  const [showStatistics, setShowStatistics] = useState(false);
+  const [positivePercentage, setPositivePercentage] = useState(0);
+
+  const changeVote = type => {
+    setShowStatistics(true);
+    switch (type) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+    }
   };
 
-  changeVote = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-      showStatistics: true,
-    }));
-  };
+  useEffect(() => {
+    setTotalFeedback(good + bad + neutral);
+    setPositivePercentage(+(100 * (good / (good + bad + neutral))).toFixed(0));
+  }, [good, bad, neutral]);
 
-  changeTotalFeedback = () =>
-    this.state.good + this.state.bad + this.state.neutral;
-
-  changePositiveFeedback = () => {
-    return +(
-      100 *
-      (this.state.good /
-        (this.state.good + this.state.bad + this.state.neutral))
-    ).toFixed(0);
-  };
-
-  render() {
-    const { good, neutral, bad, showStatistics } = this.state;
-    const totalFeedbackCount = this.changeTotalFeedback();
-    const positiveFeedbackCount = this.changePositiveFeedback();
-
-    return (
-      <>
-        <Section title="Please leave feedback" />
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={this.changeVote}
-        />
-        {showStatistics || <Notification message="There is no feedback" />}
-        {showStatistics && (
-          <>
-            <Section title="Statistics" />
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              totalFeedback={totalFeedbackCount}
-              positivePercentage={positiveFeedbackCount}
-            />
-          </>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title="Please leave feedback" />
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={changeVote}
+      />
+      {showStatistics || <Notification message="There is no feedback" />}
+      {showStatistics && (
+        <>
+          <Section title="Statistics" />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            totalFeedback={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 Feedback.propTypes = {
-  state: PropTypes.shape({
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    totalFeedback: PropTypes.number,
-    positivePercentage: PropTypes.number,
-  }),
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  totalFeedback: PropTypes.number,
+  positivePercentage: PropTypes.number,
   changeVote: PropTypes.func,
   changeTotalFeedback: PropTypes.func,
   changePositiveFeedback: PropTypes.func,
